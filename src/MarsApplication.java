f1d5a32df5e4b42f9927a1af35a508e6366d0204
BenaiminMroz <doFinansowanie911025>
2023-06-30 22:07:56 +0200

    update

--- src/MarsApplication.java
+++ src/MarsApplication.java
@@ -1,7 +1,9 @@
 public class MarsApplication {
 
     public static void main(String[] arguments) {
+
         MarsRobot spirit = new MarsRobot();
+        System.out.println("Łazik spirit");
         spirit.status = "Eksploracja";
         spirit.speed = 2;
         spirit.temperature = -60;
@@ -16,5 +18,24 @@ public class MarsApplication {
         System.out.println("Sprawdzenie temperatury.");
         spirit.checkTemperature();
         spirit.showAttributes();
+
+
+        MarsRobot opportunity = new MarsRobot();
+        System.out.println("=============================");
+        System.out.println("Łazik opportunity");
+        opportunity.status = "Eksploracja";
+        opportunity.speed = 3;
+        opportunity.temperature = -59;
+
+        opportunity.showAttributes();
+        System.out.println("Zwiększenie prędkości do 4.");
+        opportunity.speed = 4;
+        opportunity.showAttributes();
+        System.out.println("Zamiania temperatury na -80.");
+        opportunity.temperature = -80;
+        opportunity.showAttributes();
+        System.out.println("Sprawdzenie temperatury.");
+        opportunity.checkTemperature();
+        opportunity.showAttributes();
     }
 }
