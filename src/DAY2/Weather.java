e68a4d92f67d8234fef6098822ff566565e94a2e
BenaiminMroz <doFinansowanie911025>
2023-07-01 13:22:13 +0200

    day 2

---
+++ src/DAY2/Weather.java
@@ -0,0 +1,28 @@
+//package DAY2;
+//
+//public class Weather {
+//    public static void main(String[] args) {
+//
+//        //Fahrenheity na Celcjusza
+//        float fah = 86;
+//        System.out.println(fah + " stopni Fahrenheita to ...");
+//        fah = fah - 32;
+//        fah = fah / 9;
+//        fah = fah * 5;
+//        System.out.println(fah + " stopni Celcjusza\n");
+//
+//        //Celcjusza na Fahrenheity
+//        float cel = 33;
+//        System.out.println(cel + " stopnie Celcjusza to ...");
+//        cel = cel * 9;
+//        cel = cel / 5;
+//        cel = cel +32;
+//        System.out.println(cel + " stopni Fahrenheita");
+//
+////        boolean isHip;
+////        int age = 37;
+////        isHip = age < 25;
+////        System.out.println(isHip);
+//
+//    }
+//}
