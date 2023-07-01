56ea596105b74117753b7afe0c8d5a12c0585f23
BenaiminMroz <doFinansowanie911025>
2023-07-01 14:17:55 +0200

    day 3 pt.1

---
+++ src/DAY3/PointSetter.java
@@ -0,0 +1,24 @@
+//package DAY3;
+//
+//import java.awt.*;
+//
+//public class PointSetter {
+//
+//    public static void main(String[] args) {
+//        Point location = new Point(4, 13);
+//
+//        System.out.println("Położenie początkowe:");
+//        System.out.println("X równe " + location.x);
+//        System.out.println("Y równe " + location.y);
+//
+//        //System.out.println("\nPrzejście do (7, 6)");
+//        location.x = 7;
+//        location.y = 6;
+//        //much nicer way to print
+//        System.out.println("\nPrzejście do (" + location.x + ", " + location.y + ")");
+//
+//        System.out.println("Położenie końcowe:");
+//        System.out.println("X równe " + location.x);
+//        System.out.println("Y równe " + location.y);
+//    }
+//}
---
+++ src/DAY3/StringChecker.java
@@ -0,0 +1,17 @@
+package DAY3;
+
+public class StringChecker {
+
+    public static void main(String[] args) {
+
+        String str = "Lannister zawsze spłaca swoje długi.";
+        System.out.println("Tekst to: " + str);
+        System.out.println("Długośc tekstu: " + str.length());
+        System.out.println("Znak na pozycji 7.: " + str.charAt(6));
+        System.out.println("Fragment od znaku 11. do 17.: " + str.substring(10, 16));
+        System.out.println("Indeks pierwszej litery 't': " + str.indexOf('t'));
+        System.out.println("Indeks początku podtekstu " + "\"długi\": " + str.indexOf("długi"));
+        System.out.println("Tekst zapisanu wielkimi literami: " + str.toUpperCase());
+
+    }
+}
