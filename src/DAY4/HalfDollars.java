c8448be838bf331ab820e21461304b99eb4ab3d3
BenaiminMroz <doFinansowanie911025>
2023-07-04 22:08:56 +0200

    day 4 pt.2

--- src/DAY4/HalfDollars.java
+++ src/DAY4/HalfDollars.java
@@ -11,9 +11,9 @@
 //        philadelphia[1] = 5_000_000;
 //        philadelphia[2] = 2_500_000;
 //
-//        total[0] = denver[0] +philadelphia[0];
-//        total[1] = denver[1] +philadelphia[1];
-//        total[2] = denver[2] +philadelphia[2];
+//        total[0] = denver[0] + philadelphia[0];
+//        total[1] = denver[1] + philadelphia[1];
+//        total[2] = denver[2] + philadelphia[2];
 //        average = (total[0] + total[1] + total[2]) / 3;
 //
 //        System.out.print("Produkcja w 2012: ");
